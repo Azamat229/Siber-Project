c1fd08210f94fc361dc38050236b5e3bc8c59ed4
Oleg <shishkin_o@iuca.kg>
2022-11-02 22:20:29 +0600

    Initial

---
+++ Transport2/Contracts/ArrivalDTO.cs
@@ -0,0 +1,16 @@
+﻿using Contracts.EnumDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts
+{
+    public class ArrivalDTO
+    {
+        public int Id { get; set; }
+        public ArrivalTimeDTO ArrivalTime { get; set; }
+        public CampusDTO Campus { get; set; }
+    }
+}
